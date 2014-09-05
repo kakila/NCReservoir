16315a96822fdf46d271cef6f88c4903f84ced02
Federico Corradi <federico.corradi@gmail.com>
2014-09-05 20:19:06 +0200

    Update expSetup_monster_lsm.py
    experiment with no variability

--- expSetup_monster_lsm.py
+++ expSetup_monster_lsm.py
@@ -24,6 +24,7 @@
 #
 # ===============================
 #!/usr/env/python
+from __future__ import division
 
 import numpy as np
 from pylab import *
@@ -32,7 +33,6 @@ import sys
 import matplotlib
 sys.path.append('../api/lsm/')
 import lsm as L
-
 
 
 ######################################
@@ -70,8 +70,8 @@ if (is_configured == False):
   #init liquid state machine
   liquid = L.Lsm(rcnpop, cee=0.8, cii=0.5)
 
-  c = 0.3
-  dim = np.round(np.sqrt(len(liquid.rcn.synapses['virtual_exc'].addr)*c))
+  #c = 0.2
+  #dim = np.round(np.sqrt(len(liquid.rcn.synapses['virtual_exc'].addr)*c))
   
   # do config only once
   is_configured = True
@@ -81,13 +81,13 @@ if (is_configured == False):
 ######################################
 # Generate gestures parameters
 num_gestures = 1 # Number of gestures
-ntrials      = 3 # Number of repetitions of each gesture
+ntrials      = 100 # Number of repetitions of each gesture
 
 gestures = []
 for this_gesture in range(num_gestures):
-    freqs   = np.random.randint(7,size=3).tolist()   # in Hz
-    centers = np.random.random((3,2)).tolist()
-    width   = np.random.random((1,3)).tolist()
+    freqs   = (np.random.randint(3,size=3)+1).tolist()   # in Hz
+    centers = (-1+2*np.random.random((3,2))).tolist()
+    width   = (0.5+np.random.random(3)).tolist()
     gestures.append({'freq': freqs, 'centers': centers, 'width': width})
     
 import json
@@ -100,11 +100,11 @@ rates = [[]*len(gestures)]
 G     = [[]*len(gestures)]
 for ind,this_g in enumerate(gestures):
   for f  in this_g['freq']:
-      rates[ind].append(lambda t,w=f: 0.5+0.5*np.sin(2*np.pi*w*t*1e-3)) # time in ms
+      rates[ind].append(lambda t,w=f: 0.5+0.5*np.sin(2*np.pi*w*t)) 
 
   # Multiple spatial distribution
   for width,pos in zip(this_g['width'], this_g['centers']):
-      G[ind].append(lambda x,y,d=width,w=pos: np.exp ((-(x-w[0])**2 + (y-w[1])**2)/(np.sum(width)**2)))
+      G[ind].append(lambda x,y,d=width,w=pos: np.exp ((-(x-w[0])**2 + (y-w[1])**2)/d**2))
 
 # Number of time steps to sample the mean rates
 nsteps = 50
@@ -120,6 +120,7 @@ ion()
 
 # Store scores of RC
 scores = []
+tot_scores = []
 # Stimulation parameters
 duration   = 1000
 delay_sync = 500
@@ -131,52 +132,71 @@ nT    = np.round (Fs*T)
 timev = np.linspace(0,T,nT)
 
 #Conversion from spikes to analog
-membrane = lambda t,ts: np.atleast_2d(np.exp((-(t-ts)**2)/(2*50**2)))
+membrane = lambda t,ts: np.atleast_2d(np.exp((-(t-ts)**2)/(2*30**2)))
 
 liquid.RC_reset()
+ac = []
+syn_per_input_neu = 6
+c = syn_per_input_neu/len(liquid.rcn.synapses['virtual_exc'].addr)
 for ind,this_g in enumerate(gestures):
 
-    M = liquid.create_stimuli_matrix(G[ind], rates[ind], nsteps)
+    M = liquid.create_stimuli_matrix(G[ind], rates[ind], nsteps, nx=2,ny=2)
     #one stimiluation for  all trials --> NO VARIABILITY IN THE INPUTs
-    stimulus = liquid.create_spiketrain_from_matrix(M, \
-                                                    duration=duration, \
-                                                    delay_sync=delay_sync)
+    stimulus = liquid.create_spiketrain_from_matrix(M, 
+                                                    c = c, 
+                                                    duration=duration,  
+                                                    delay_sync=delay_sync,  
+                                                    max_freq= 2500, min_freq = 350)
     
     #generate teaching signal associated with the Gesture
-    gesture_teach = rates[0][ind](timev)
+    teach_scale = arange(0.1,2,0.05)
+    gesture_teach = rates[0][ind]((teach_scale*timev[:,None])*1e-3)
     
     for this_t in xrange(ntrials): 
+    
+        #stimulus = liquid.create_spiketrain_from_matrix(M, 
+        #                                            c = c, 
+        #                                            duration=duration,  
+        #                                            delay_sync=delay_sync,  
+        #                                            max_freq= 2500, min_freq = 350)
+    
+    
         nsetup.chips['mn256r1'].load_parameters('biases/biases_reservoir.biases')    
         #stimulate
         inputs, outputs = liquid.RC_poke(stimulus)
 
-        #if(learn_real_time == True):
-        # Calculate activity of current inputs.
-        # As of now the reservoir can only give answers during activity
-        ac = np.mean(func_avg(timev[:,None], outputs[0][:,0][None,:]), axis=1) 
-        ac = ac / np.max(ac)
-        ac = ac[:,None]
-        
         # Convert input and output spikes to analog signals
         X = L.ts2sig(timev, membrane, inputs[0][:,0], np.floor(inputs[0][:,1]))
         Y = L.ts2sig(timev, membrane, outputs[0][:,0], outputs[0][:,1])
-        #if(this_t >0):
-        #    print "X vs Vpre", np.sum(np.abs(Xprev-X))
-        #Xprev = X 
+        
+        #if(learn_real_time == True):
+        # Calculate activity of current inputs.
+        # As of now the reservoir can only give answers during activity
+        tmp_ac = np.mean(func_avg(timev[:,None], outputs[0][:,0][None,:]), axis=1) 
+        tmp_ac = tmp_ac / np.max(tmp_ac)
+        if (np.sum(tmp_ac) > np.sum(ac)) or (this_t==0):   
+            ac = tmp_ac[:,None]
 
-        teach_sig = gesture_teach * ac.T**4 # Windowed by activity
+        #teach_sig = L.orth_signal(X)*ac.T**4 #
+        #teach_sig = L.orth_signal(X)[None,:]
+        teach_sig = gesture_teach * ac**4 # Windowed by activity
 
         #learn
-        liquid._realtime_learn (X,Y,teach_sig.T)
-
+        liquid._realtime_learn (X,Y,teach_sig)
+        print np.sum(liquid.CovMatrix['input']), np.sum(liquid.CovMatrix['output'])
         #evaluate
-        this_score = [liquid._regressor["input"].score(X,teach_sig.T), \
-                     liquid._regressor["output"].score(Y,teach_sig.T)]
-        scores.append(this_score)
-
-        print "we are scoring..."
-        print this_score
-        print "we are plotting outputs"
+        zh = liquid.RC_predict (X,Y)
+        score_in = liquid.RC_score(zh["input"], teach_sig)
+        score_out = liquid.RC_score(zh["output"], teach_sig)
+        
+        tot_scores.append([score_in, score_out])
+        #print "we are scoring...", scores
+                
+        #this_score = [liquid._regressor["input"].score(X,teach_sig)], \
+        #             liquid._regressor["output"].score(Y,teach_sig)]
+   
+        #print this_score
+        #print "we are plotting outputs"
         #figure(fig_h.number)
         #for i in range(256):
         #    subplot(16,16,i)
@@ -189,20 +209,62 @@ for ind,this_g in enumerate(gestures):
         #    plot(X[:,i])
         #    axis('off')
 
+         
             
 
+#figure()
+#plot(np.array(scores)[:,0], 'o-', label='input')
+#plot(np.array(scores)[:,1], 'o-', label='output')
+#ylim([-1,1])
+#legend(loc='best')
+tot_in_scores = np.zeros([np.shape(teach_sig)[1], 2])
+tot_out_scores = np.zeros([np.shape(teach_sig)[1], 2])
+for i in range(5):
+    inputs, outputs = liquid.RC_poke(stimulus)
+    # Convert input and output spikes to analog signals
+    X = L.ts2sig(timev, membrane, inputs[0][:,0], np.floor(inputs[0][:,1]))
+    Y = L.ts2sig(timev, membrane, outputs[0][:,0], outputs[0][:,1])
+    zh = liquid.RC_predict (X,Y)
+    pred_in_scores = liquid.RC_score(zh["input"], teach_sig)
+    pred_out_scores = liquid.RC_score(zh["output"], teach_sig)
+    tot_in_scores += pred_in_scores[:,None]
+    tot_out_scores += pred_out_scores[:,None]
+tot_in_scores /= 5
+tot_out_scores /= 5
+
 figure()
-plot(np.array(scores)[:,0], 'o-', label='input')
-plot(np.array(scores)[:,1], 'o-', label='output')
-legend(loc='best')
+plot(teach_scale, tot_in_scores, 'o-')
+xlabel("scale [au]")
+ylabel("score ")
+title("input performances")
+
+figure()
+plot(teach_scale, tot_out_scores, 'o-')
+xlabel("scale [au]")
+ylabel("score ")
+title("output performances")
 
 figure()
 zh = liquid.RC_predict (X,Y)
 clf()
-plot(timev,teach_sig.T,label='teach signal')
-plot(timev,zh["input"], label='input')
-plot(timev,zh["output"], label='output')
+for i in range(len(teach_scale)):
+    subplot(7,7,i+1)
+    plot(timev,teach_sig[:,i],label='teach signal')
+    plot(timev,zh["input"][:,i], label='input')
+    plot(timev,zh["output"][:,i], label='output')
 legend(loc='best')
+
+#check non linearity
+#figure()
+#a,b = np.where(X>0)
+#plot(X[:,b[0]], Y, 'o')
+#figure()
+#a,b = np.where(X>0)
+#plot(X[:,b[1]], Y, 'o')
+
+
+
+
 
 
 
